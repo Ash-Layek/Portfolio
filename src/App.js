89b5d098ac0b2aea0177a337599419963126e198
Achraf <achraf.layek1@gmail.com>
2023-06-07 15:20:58 -0700

    Navbar Component imported

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from "./components/Home/Home"
 import Projects from "./components/Projects/Projects"
 import Contact from "./components/Contact/Contact"
+import Navbar from "./components/Navbar/NavBar.js"
 
 function App() {
   return (
